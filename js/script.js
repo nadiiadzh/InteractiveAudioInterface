f98325254753cbd79e71c5552e88d973d0bf3cf8
Nadiia Dzhyhryniuk <nadiadzh7@gmail.com>
2020-12-19 22:49:50 -0500

    fixed issue with previous and next buttons

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
-var count = null; //  flag for current song
+let count = 0; //  flag for current song
+let playingSong = false;
 
-var songs = ['track1','track2','track3','track4'];// array with names of songs
+let songs = ['track1','track2','track3','track4'];// array with names of songs
+
 
 window.onload = function(){
 
     var mp3 = document.getElementById("mp3"); //reference to the audio
 
-    document.querySelector(`.next`).onclick = next; // reference to the buttons
-    document.querySelector(`.stop`).onclick = stop;
-    document.querySelector(`.play`).onclick = play;
-    document.querySelector(`.prev`).onclick = prev;
+//adding event listener to the buttons 
+
+    let playBtn = document.querySelector(`.play`)
+    playBtn.addEventListener('click', play)
+  
+    let pauseBtn = document.querySelector(`.stop`)
+    pauseBtn.addEventListener('click', stop)
+
+    let nextBtn = document.querySelector(`.next`)
+    nextBtn.addEventListener('click', next)
+  
+    let prevBtn = document.querySelector(`.prev`)
+    prevBtn.addEventListener('click', prev)
 
 //adding list of songs names
+
     var ul = document.getElementById("ul");
 
     for(var i = 0; i < songs.length; i++){
@@ -20,7 +32,9 @@ window.onload = function(){
         ul.appendChild(li);
     }
 }
+
 // define the current song, adding active class which change the background color in a
+    
     function changeColor(){
 
         var color = document.getElementsByTagName('li');
@@ -39,18 +53,33 @@ function next(){
     if(count < songs.length-1){
         count++;
         play();
+    }else{
+        count = 0;
+        count--;
     }
 }
+
 function prev(){
     if(count > 0){
         count--;
         play();
     }
 }
+
+function justplay(){
+    if(playingSong == false){
+        play();
+    }else{
+        stop();
+    }
+
+}
+
 function play(){
         changeColor();
         mp3.src = "audio/"+songs[count]+".mp3";      
 }
+
 function stop(){
     mp3.pause();
 }
