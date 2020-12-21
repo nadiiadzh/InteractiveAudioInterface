3e97b7f9bc30cf6901b210b9c7aa61618afe8636
Nadiia Dzhyhryniuk <nadiadzh7@gmail.com>
2020-12-20 20:50:48 -0500

    creating array with song objects,  adding loadTrack() and justplay() functions for loading song  and checking is song playing.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,43 @@
 let count = 0; //  flag for current song
 let playingSong = false;
 
-let songs = ['track1','track2','track3','track4'];// array with names of songs
+// array of objects with songs
+let songs = [
+    {
+        name: 'track 1',
+        path: 'audio/track1.mp3',
+        img:   'img/img1.jpg',
+        singer: 'First singer'
+    },
+    {
+        name: 'track 2',
+        path: 'audio/track2.mp3',
+        img:   'img/img2.jpg',
+        singer: 'Second singer'
+    },
+    {
+        name: 'track 3',
+        path: 'audio/track3.mp3',
+        img:   'img/img3.jpg',
+        singer: 'Third singer'
+    },
+    {
+        name: 'track 4',
+        path: 'audio/track4.mp3',
+        img:   'img/img4.jpg',
+        singer: 'Fourth singer'
+    }
+];
 
 
 window.onload = function(){
 
-    var mp3 = document.getElementById("mp3"); //reference to the audio
-
+   var mp3 = document.getElementById("mp3"); //reference to the audio
+ 
 //adding event listener to the buttons 
 
     let playBtn = document.querySelector(`.play`)
-    playBtn.addEventListener('click', play)
+    playBtn.addEventListener('click', justplay)
   
     let pauseBtn = document.querySelector(`.stop`)
     pauseBtn.addEventListener('click', stop)
@@ -28,16 +54,16 @@ window.onload = function(){
 
     for(var i = 0; i < songs.length; i++){
         var li = document.createElement("li");
-        li.innerHTML = songs[i];
+        li.innerHTML = songs[i].name;
         ul.appendChild(li);
     }
 }
 
 // define the current song, adding active class which change the background color in a
     
-    function changeColor(){
+function changeColor(){
 
-        var color = document.getElementsByTagName('li');
+    var color = document.getElementsByTagName('li');
 
         for(var x=0; x < songs.length; x++){
             if(count == x){
@@ -47,42 +73,57 @@ window.onload = function(){
             }
         }
 
+}
+
+function loadTrack(){
+
+    mp3.src = songs[count].path;
+    mp3.load();
+
+}
+
+function justplay(){
+    if(playingSong == false){
+        play();
+    }else{   
+       stop();
     }
+}
+
+function play(){
+    changeColor();
+    mp3.play();
+    playingSong = true;      
+}
 
 function next(){
     if(count < songs.length-1){
         count++;
+        loadTrack(count);
         play();
     }else{
         count = 0;
         count--;
+        loadTrack(count);
+        play();
     }
 }
 
 function prev(){
     if(count > 0){
         count--;
-        play();
+        loadTrack(count);
+        play()
     }else{
         count = songs.length;
-        play();
+
+        loadTrack(count);
+        play()
     }
-}
-
-function justplay(){
-    if(playingSong == false){
-        play();
-    }else{
-        stop();
-    }
-
-}
-
-function play(){
-        changeColor();
-        mp3.src = "audio/"+songs[count]+".mp3";      
 }
 
 function stop(){
     mp3.pause();
+    playingSong = false;
 }
+
